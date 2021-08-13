3830fa6c99557da34bf5be36c72b6e42015a94a0
nofarAv1 <nofar8287@gmail.com>
2021-08-13 20:34:17 +0300

    edit flaskwrapper file

--- MessagingSystemMission/flask_wrapper/run.py
+++ MessagingSystemMission/flask_wrapper/run.py
@@ -5,19 +5,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to Flask Wrapper"
+    return "Welcome to Messages system"
 
 # flask 1 is messages_handler
-@app.route("/flask1/")
-@app.route("/flask1/<path:path>")
+@app.route("/message/")
+@app.route("/message/<path:path>")
 def flask1(path=""):
     return request.urlopen("http://localhost:5001/" + path).read()
 
-# # flask2 is db_handler
-# @app.route("/flask2/")
-# @app.route("/flask2/<path:path>")
-# def flask2(path=""):
-#     return request.urlopen("http://localhost:5002/" + path).read()
+# flask2 is db_handler
+@app.route("/db/")
+@app.route("/db/<path:path>")
+def flask2(path=""):
+    return request.urlopen("http://localhost:5002/" + path).read()
 
 
 if __name__ == "__main__":

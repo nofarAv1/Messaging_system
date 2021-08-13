2168ecd3b0fdcafd37d0328cec3495baa075054b
nofarAv1 <nofar8287@gmail.com>
2021-08-13 20:01:49 +0300

    edit flaskwrapper for get db handler via messages_system

--- MessagingSystemMission/flask_wrapper/run.py
+++ MessagingSystemMission/flask_wrapper/run.py
@@ -8,16 +8,16 @@ def home():
     return "Welcome to Flask Wrapper"
 
 # flask 1 is messages_handler
-@app.route("/flask1/")
-@app.route("/flask1/<path:path>")
+@app.route("/")
+@app.route("/<path:path>")
 def flask1(path=""):
     return request.urlopen("http://localhost:5001/" + path).read()
 
-# flask2 is db_handler
-@app.route("/flask2/")
-@app.route("/flask2/<path:path>")
-def flask2(path=""):
-    return request.urlopen("http://localhost:5002/" + path).read()
+# # flask2 is db_handler
+# @app.route("/flask2/")
+# @app.route("/flask2/<path:path>")
+# def flask2(path=""):
+#     return request.urlopen("http://localhost:5002/" + path).read()
 
 
 if __name__ == "__main__":
